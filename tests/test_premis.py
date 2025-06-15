74608a1b03db33c86dcad6433fde6f09564bcd35
Luca Soto De Graeve <lucasotodegraeve@gmail.com>
2025-06-15 16:51:48 +0200

    Rename tree serialization function

--- tests/test_premis.py
+++ tests/test_premis.py
@@ -2,7 +2,7 @@ from xmlschema import XMLSchema
 import pytest
 
 import xsd2xml
-from tests.utils import tree_to_str
+from tests.utils import serialize_tree
 
 PREMIS = "tests/assets/premis.xsd.xml"
 
@@ -14,9 +14,9 @@ def premis_xsd() -> XMLSchema:
 
 def test_agent_identifier_valid(premis_xsd: XMLSchema):
     doc = xsd2xml.generate(PREMIS, "agentIdentifier")
-    assert premis_xsd.is_valid(tree_to_str(doc))
+    assert premis_xsd.is_valid(serialize_tree(doc))
 
 
 def test_agent(premis_xsd: XMLSchema):
     doc = xsd2xml.generate(PREMIS, "agent")
-    assert premis_xsd.is_valid(tree_to_str(doc))
+    assert premis_xsd.is_valid(serialize_tree(doc))
--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,7 @@ import io
 import xml.etree.ElementTree as ET
 
 
-def tree_to_str(tree: ET.ElementTree) -> str:
+def serialize_tree(tree: ET.ElementTree) -> str:
     data = io.StringIO()
     tree.write(data, encoding="unicode")
     return data.getvalue()
