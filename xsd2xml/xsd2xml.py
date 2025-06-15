63eff931150a60a90339b80a869a4f11dd0e437b
Luca Soto De Graeve <lucasotodegraeve@gmail.com>
2025-06-15 17:16:11 +0200

    Exctract get inlined type function

--- xsd2xml/xsd2xml.py
+++ xsd2xml/xsd2xml.py
@@ -56,17 +56,19 @@ def _is_type_definition_inlined(xsd_element: _Element) -> bool:
     return xsd_element.get_resolved_attribute("type") is None
 
 
+def _get_inlined_type_definition(xsd_element: _Element) -> _Element:
+    return next(
+        el for el in xsd_element.children if el in (XSD.complex_type, XSD.simple_type)
+    )
+
+
 def _find_type_definition(xsd_element: _Element) -> _Element | BuiltInType:
     type = xsd_element.get_resolved_attribute("type")
     if type in BuiltInType:
         return BuiltInType(type)
 
     if _is_type_definition_inlined(xsd_element):
-        return next(
-            el
-            for el in xsd_element.children
-            if el in (XSD.complex_type, XSD.simple_type)
-        )
+        return _get_inlined_type_definition(xsd_element)
 
     simple_type = xsd_element.root.find(f"xsd:simpleType[@name='{type}']")
     if simple_type is not None:
